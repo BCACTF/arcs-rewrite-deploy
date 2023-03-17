c9176f7b7f3afe55722019088d22a2ca164023d3
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2023-03-17 15:57:29 -0400

    Begin to set up arcs-yaml-verifier

---
+++ arcs-yaml-verifier/src/categories/structs.rs
@@ -0,0 +1,72 @@
+use std::fmt::Debug;
+
+#[allow(unused)]
+static CATEGORY_DEFAULTS: [&str; 5] = ["misc", "binex", "foren", "crypto", "webex"];
+
+use lazy_static::lazy_static;
+use std::collections::HashSet;
+lazy_static!{
+    static ref CATEGORY_RAW_ENV_VAR: Option<String> = std::env::var("CATEGORIES").ok();
+
+    pub static ref CATEGORIES: HashSet<String> = CATEGORY_RAW_ENV_VAR.as_ref().map_or_else(
+        || CATEGORY_DEFAULTS.into_iter().map(str::to_string).collect(),
+        |val| val.split(",").map(|part| part.trim().to_string()).collect(),
+    );
+}
+
+#[derive(PartialEq)]
+pub struct Category {
+    name: &'static str,
+}
+
+impl Category {
+    pub fn try_new(name: &'_ str) -> Option<Self> {
+        CATEGORIES.get(name).map(|name| Self { name })
+    }
+}
+
+impl Debug for Category {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Category< {} >", self.name)
+    }
+}
+
+#[derive(PartialEq)]
+pub struct Categories(Vec<Category>);
+
+impl Categories {
+    pub fn try_new<'a>(category_names: impl IntoIterator<Item = &'a str>) -> Result<Categories, Vec<&'a str>> {
+        let mut good_cats = vec![];
+        let mut invalid_cat_names = vec![];
+
+        for name in category_names {
+            if let Some(cat) = Category::try_new(name) {
+                good_cats.push(cat);
+            } else {
+                invalid_cat_names.push(name);
+            }
+        }
+
+        if invalid_cat_names.len() > 0 {
+            Err(invalid_cat_names)
+        } else {
+            Ok(Self(good_cats))
+        }
+    }
+}
+
+impl Debug for Categories {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Categories< ")?;
+        if let Some(cat) = self.0.first() {
+            write!(f, "{}", cat.name)?;
+
+            for cat in self.0.iter().skip(1) {
+                write!(f, ", {}", cat.name)?;
+            }
+        }
+        write!(f, " >")
+    }
+}
+
+
--- arcs-yaml-verifier/src/lib.rs
+++ arcs-yaml-verifier/src/lib.rs
@@ -1,36 +1,156 @@
-use serde::{Serialize, Deserialize};
-use serde_yaml::Error as YamlError;
+pub mod categories;
+pub mod structs;
 
-use std::fmt::Display;
-use std::collections::HashSet;
+use serde_yaml::Mapping as YamlMapping;
+use serde_yaml::Value as YamlValue;
+use structs::ValueType;
 
-use lazy_static::lazy_static;
+use categories::Categories;
+use categories::CategoryError;
+use structs::{ YamlVerifyError, YamlAttribVerifyError };
+use structs::get_type;
 
-#[allow(unused)]
-static CATEGORY_DEFAULTS: [&str; 5] = ["misc", "binex", "foren", "crypto", "webex"];
 
-lazy_static! {
-    static ref CATEGORIES: Box<[&'static str]> = Box::new([]);
-}
-
-pub enum Category {
-
-}
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(PartialEq, Debug)]
 pub struct YamlShape {
     name: String,
-    categories: HashSet<String>,
+    categories: Categories,
+    points: u64,
+    description: String,
+
+    // hints: StringList,
+    // authors: StringList,
+
+    visible: bool,
 }
 
-impl Display for YamlShape {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{:#?}", self))
+
+
+macro_rules! collect_errors {
+    ($($vals:ident),+ $(,)?) => {
+        collect_errors!(@impl left: $($vals,)+; good: []; errors: [])
+    };
+    (@impl left: $val:ident, $($next_vals:ident,)*; good: [$($good_exprs:expr,)*]; errors: [$($err_exprs:expr,)*]) => {
+        match &$val {
+            Ok(_)  => collect_errors!(@impl left: $($next_vals,)*; good: [$($good_exprs,)* $val.unwrap(),]; errors: [$($err_exprs,)*]),
+            Err(_) => collect_errors!(@impl left: $($next_vals,)*; good: [$($good_exprs,)*]; errors: [$($err_exprs,)* $val.unwrap_err(),]),
+        }
+    };
+    (@impl left: ; good: [$($good_exprs:expr,)*]; errors: []) => {
+        Ok(($($good_exprs,)*))
+    };
+    (@impl left: ; good: [$($good_exprs:expr,)*]; errors: [$($err_exprs:expr,)*]) => {
+        Err(vec![$($err_exprs,)*])
+    };
+}
+
+
+pub fn verify_yaml(yaml_text: &str) -> Result<YamlShape, YamlVerifyError> {
+    use YamlVerifyError::*;
+    use YamlAttribVerifyError::*;
+
+    let base: YamlValue = serde_yaml::from_str(yaml_text).map_err(Unparsable)?;
+    let base: &YamlMapping = if let Some(base) = base.as_mapping() { base } else {
+        return Err(BaseNotMap(get_type(&base)))
+    };
+
+    let categories = base
+        .get("categories")
+        .map(categories::value_to_categories)
+        .unwrap_or(Err(CategoryError::MissingKey))
+        .map_err(Categories);
+
+    let name = if let Some(name_val) = base.get("name") {
+        name_val
+            .as_str()
+            .map(str::to_string)
+            .ok_or_else(|| NameNotString(get_type(name_val)))
+    } else {
+        Err(NameNotString(ValueType::NULL))
+    };
+
+
+    let points = if let Some(point_val) = base.get("value") {
+        point_val
+            .as_u64()
+            .ok_or_else(|| PointsNotInt(get_type(point_val)))
+    } else {
+        Err(PointsNotInt(ValueType::NULL))
+    };
+
+
+    let description = if let Some(desc_val) = base.get("description") {
+        desc_val
+            .as_str()
+            .map(str::to_string)
+            .ok_or_else(|| DescNotString(get_type(desc_val)))
+    } else {
+        Err(DescNotString(ValueType::NULL))
+    };
+
+
+    let visible = if let Some(point_val) = base.get("visible") {
+        point_val
+            .as_bool()
+            .ok_or_else(|| VisNotBool(get_type(point_val)))
+    } else {
+        Err(VisNotBool(ValueType::NULL))
+    };
+    // println!("cat: {categories:#?}, name: {name:#?}");
+
+    let (
+        categories,
+        name,
+        points,
+        description,
+        visible,
+    ) = collect_errors!(
+        categories,
+        name,
+        points,
+        description,
+        visible,
+    ).map_err(PartErrors)?;
+
+    Ok(YamlShape { categories, name, points, description, visible })
+}
+
+pub mod yaml_logging_utils {
+    use arcs_deploy_shared_structs::shortcuts::*;
+
+    pub fn write_path(target: &mut impl std::io::Write, path: &str) -> IOResult<()> {
+        writeln!(target, "{:-^30}", path)
     }
 }
 
-pub fn verify_yaml(yaml_text: &str) -> Result<YamlShape, YamlError> {
-    serde_yaml::from_str(yaml_text)
+#[doc(hidden)]
+pub mod __main {
+
+    pub fn main() {
+        let mut errors_encountered = false;
+
+        std::env::args()
+            .skip(1)
+            .filter_map(
+                |path| {
+                    println!("{:-^30}", path);
+                    match std::fs::read_to_string(&path) {
+                        Ok(string) => Some(string),
+                        Err(_err) => {
+                            println!("Failed to read `{}` to string. Check location, permissions, and encoding of the file.", path);
+                            None
+                        },
+                    }
+                }
+            )
+            .for_each(
+                |yaml_parse_result| match crate::verify_yaml(&yaml_parse_result) {
+                    Ok(yaml) => println!("{:#?}", yaml),
+                    Err(err) => {
+                        errors_encountered = true;
+                        eprintln!("{}", err);
+                    },
+                }
+            );
+    }
 }
-
-
